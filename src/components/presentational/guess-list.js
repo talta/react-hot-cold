f6c80cf159d2b40d805b2a4d955a92ea8a9d958a
talta <t.taltallc@gmail.com>
2017-03-29 15:27:49 -0400

    attempted numbers to return

--- src/components/presentational/guess-list.js
+++ src/components/presentational/guess-list.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 export default function GuessList(props)  {
+	const guesses = props.guesses.map{(guess, index) =>(
+		<li key={index}>
+			{guess}
+		</li>
+	)}
+
     return (
       <div>
-        <ul id="guessList">
-          "props.attemptedNumbers"
+        <ul id="guessList" className="guessList clearFix">
+
+          {guesses}
         </ul>
       </div>
     );
 }
+
+          // {props.attemptedNumbers} removed from the return in favor of thinkful's better suggestion to map the attemptedNumbers here
