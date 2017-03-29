5fb7bc8929ba0c1e7ef1efd41128376e38c20146
talta <t.taltallc@gmail.com>
2017-03-29 15:05:22 -0400

    instructions and overview of react

---
+++ src/components/presentational/info-modal.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function Form(props){
+	onClose(event){
+		event.preventDefault;
+		if(this.props.onClose){
+			this.props.onClose();
+		}
+	}
+	render(){
+		return(
+			<div>
+				<div>
+					<h3>What do I do is the question</h3>
+					<div>
+						<p>This is a game completed in React. The game is simple, but react can be confusing.</p>
+						<ul>
+							<li> project setup is complex, especially when learning</li>
+							<li> breaking the structure and interactivity is like breaking the HTML and jQuery model previously taught/ learned</li>
+							<li> I feel like too much of a noobie to learn functional programming, but here I am</li>
+						</ul>
+					</div>
+					<p> So are you ready to interact with my React application?</p>
+					<a className= "close" href="#" onClick={e => this.onClose(e)}>Let's do it!</a>
+				</div>
+			</div>
+		);
+	}
+
+
+}
