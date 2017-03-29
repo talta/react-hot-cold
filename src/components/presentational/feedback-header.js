a7e5171b6b533b063c23960a46ea72b6084c7f06
talta <t.taltallc@gmail.com>
2017-03-29 15:21:59 -0400

    feedback header used as the guess section

--- src/components/presentational/feedback-header.js
+++ src/components/presentational/feedback-header.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import Form from './form';
+
 export default function FeedbackHeader(props)  {
     return (
-      <h2 id="feedback"></h2>
+    	<section>
+     		<h2 id="feedback"></h2>
+     		<Form onGuess={props.onGuess} />
+     	</section>
     );
 }
 
