119d367c30baf56c88fa4b2d4e3499add771efe6
talta <t.taltallc@gmail.com>
2017-03-29 15:39:56 -0400

    the form completed

--- src/components/presentational/form.js
+++ src/components/presentational/form.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
 export default function Form(props){
+  	onGuess(event){
+  		event.preventDefault();
+
+  		///if the props onGuess exists, then set the value that was entered into the Guess area
+  		if(this.props.onGuess){
+  			const value = this.input.value;
+  			this.props.onGuess(value);
+  		}
+  		////then afterwards set the value to be empty
+  		this.input.value = '';
+  	}
+  	/////I wasnt far off with the return formatting:
+  	////attach the event listener to the form
+  	////ref will be the input into the input function, being set to the input
+  	
+  	///i do not believe that a render function needs to preceed the return 
   	return (
       <div className="formArea">
-      	<form id="form">
-      	<input type="text" name="userGuess" id="userGuess" className="text"  />
-      	<input type="submit" id="guessButton" className="button" name="submit" value="Guess"  />
+      	<form id="form" onSubmit={e=> this.onGuess(e)}>
+      		<input type="text" name="userGuess" id="userGuess" className="text"  
+      			placeholder="Enter your Guess" required
+      			ref{input=> this.input =input}/>
+      		<input type="submit" id="guessButton" className="button" name="submit" value="Guess"  />
       	</form>
       </div>
   	)
