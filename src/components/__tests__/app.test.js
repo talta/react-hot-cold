991dfe68f54e151b6ace4acf820e9b7d99e9223f
talta <t.taltallc@gmail.com>
2017-04-01 12:34:59 -0400

    app js tests

---
+++ src/components/__tests__/app.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+import TestUtils from 'eact-addons-test-utils';
+
+import App from '../../App.js'
+
+describe('<App />', ()=>{
+	it('should render without crashing', ()=>{
+		shallow(<App />);
+	});
+	it('should have a method for newGame', ()=>{
+		const wrapper = shallow(<App />);
+		wrapper.instance().newGame(true);
+	});
+	it('should have a method for guess', ()=>{
+		const wrapper = shallow(<App />);
+		wrapper.instance().guess(true);
+	});
+});
