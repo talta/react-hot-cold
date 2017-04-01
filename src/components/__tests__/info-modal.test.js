5de9cb4a6197e1c9f9caff06fe8ce73046024281
talta <t.taltallc@gmail.com>
2017-04-01 11:40:38 -0400

    tests for info modal

--- src/components/__tests__/info-modal.test.js
+++ src/components/__tests__/info-modal.test.js
@@ -7,4 +7,15 @@ describe('<InfoModal />', ()=>{
 	it('should render without crashing', ()=>{
 		shallow(<InfoModal />);
 	});
+	it('should have the method for onClose', ()=>{
+		const wrapper = shallow(<InfoModal />);
+		wrapper.instance().onClose(true);
+	});
+	it('should have a classname of close', ()=>{
+		const wrapper = shallow(<InfoModal />);
+		expect(wrapper.hasClass('close')).toEqual(true);
+	});
+	////it('should not render when the close method is selected', ()=>{
+/////i am unsure how to do this through enzyme 
+	///});
 });
